919a3a1f58a0c387331ef85b61ba1bbef1305f24
cobrianz <briancheruiyot.h@gmail.com>
2023-11-24 23:23:30 +0300

    first commit

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ setTimeout(function() {
     }, 2500);
   }, 2000);
 
-  
+  /* form */
+
+  const signup = document.querySelector('.form__signup');
+  const signin = document.querySelector('.form__login');
+  const signupBtn = document.querySelectorAll('.button__light');
+  const signinBtn = document.querySelectorAll('.button__light');
+
+
+function toggleForm() {
+  signup.style.right = "0%";
+  signin.style.left = "-100%";
+}
+function toggleform() {
+  signin.style.left = "50%";
+  signup.style.right = "-100%";
+}
